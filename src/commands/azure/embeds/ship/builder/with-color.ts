98649e7654330b17bd617a2fa9e10a106b3eb9e2
Hououin Kyouma <inwazzin@gmail.com>
2021-09-20 00:45:33 +0200

    embed constructor

--- src/commands/azure/embeds/ship/builder/with-color.ts
+++ src/commands/azure/embeds/ship/builder/with-color.ts
@@ -1,7 +1,16 @@
 import { WithFn } from './types';
 import { ColorResolvable } from 'discord.js';
 
-const colorByStars = (stars: number): ColorResolvable => '#ffffff';
+const colorByStars: Record<number, ColorResolvable> = {
+  0: '#FFFF00',
+  1: '#FF00FF',
+  2: '#00FFFF',
+  3: '#0F0FFF',
+  4: '#0F0F0F',
+  5: '#00F000',
+  6: '#F00000',
+  7: '#F0F00F',
+};
 
 export const withColor: WithFn = (builder) => {
   const {
@@ -11,6 +20,6 @@ export const withColor: WithFn = (builder) => {
     embed,
   } = builder;
 
-  embed.setColor(colorByStars(stars));
+  embed.setColor(colorByStars[stars]);
   return builder;
 };
